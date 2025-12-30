e7b06ab7228f62f4034c64f14d26cac2014e1e4b
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2025-12-31 04:19:13 +0900

    chore: storybook에 tailwind 적용을 위한 config 파일 확장

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,11 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./.storybook/**/*.{js,ts,jsx,tsx,mdx}", // 스토리북 설정/문서
+    "./src/**/*.stories.{js,ts,jsx,tsx,mdx}", // 스토리 파일
+  ],
   plugins: [],
 } satisfies Config;
