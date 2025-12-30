562d1728899a78e1f1d74ba6ec3088d5c1fe52ac
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2025-12-30 17:17:15 +0900

    chore: 불필요한 import 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import "./index.css";
-import "./styles/tailwind.css";
 import "./styles/tokens.css";
 
 import { router } from "./router";
