1fd1cb7f0233edf04adc1b3e05352d3ae56283fb
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2025-12-30 13:56:06 +0900

    chore: 컬러 팔레트 tailwind 연동

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,33 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          0: "var(--gray-0)",
+          10: "var(--gray-10)",
+          20: "var(--gray-20)",
+          30: "var(--gray-30)",
+          40: "var(--gray-40)",
+          50: "var(--gray-50)",
+          60: "var(--gray-60)",
+          70: "var(--gray-70)",
+          80: "var(--gray-80)",
+        },
+        main: {
+          DEFAULT: "var(--main)",
+          soft: "var(--main-soft)",
+          accent: "var(--main-accent)",
+          muted: "var(--main-muted)",
+        },
+        flame: {
+          0: "var(--flame-0)",
+          30: "var(--flame-30)",
+          60: "var(--flame-60)",
+          100: "var(--flame-100)",
+        },
+      },
+    },
   },
   plugins: [],
 };
