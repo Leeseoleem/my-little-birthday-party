d9f6bd802a034cdc2521d6812fd82de23c731c63
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2025-12-30 12:50:47 +0900

    chore: __root 파일 형식에 맞게 수정

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,15 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
-export const RootRoute = createRootRoute({
+export const Route = createRootRoute({
   component: RootLayout,
 });
 
 function RootLayout() {
   return (
-    <>
+    <div className="flex flex-col h-full bg-gray-100">
       <Outlet />
       <TanStackRouterDevtools position="bottom-right" />
-    </>
+    </div>
   );
 }
