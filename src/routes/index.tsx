298f92648f1d9cda6803ac9652914b03916a3258
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-01 23:20:28 +0900

    chore: 테스트 용 코드

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,60 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { useState } from "react";
+
+import AppHeader from "../components/ui/Header/AppHeader";
+import { BackButton } from "../components/ui/Header/BackButton";
+import PageTitle from "../components/ui/PageTitle";
+import TextInput from "../components/ui/Input/TextInput";
+import CommonButton from "../components/ui/Button/Button";
+import BirthDateInput from "../components/ui/Input/BirthDateInput";
+import FormField from "../components/Form/FormField";
+
 export const Route = createFileRoute("/")({
   component: HomePage,
 });
 
 function HomePage() {
+  const [inputValue, setInputValue] = useState("");
+  const [birthValue, setBirthValue] = useState("");
   return (
     <div className="flex flex-col">
-      <p className="text-letter-base">폰트 스타일 확인용</p>
-      <p className="text-letter-night">폰트 스타일 확인용</p>
-      <p className="text-letter-system">폰트 스타일 확인용</p>
-      <p className="text-letter-base-mobile">폰트 스타일 확인용</p>
-      <p className="text-letter-night-mobile">폰트 스타일 확인용</p>
-      <p className="text-letter-system-mobile">폰트 스타일 확인용</p>
+      <AppHeader
+        left={<BackButton fallbackTo="/" />}
+        progress={{
+          value: 0.6,
+        }}
+      />
+      <PageTitle
+        title="홈페이지"
+        subTitle="이제, 이 초대장이 누구에게 갈지 알려주세요"
+      />
+
+      <FormField
+        id="name"
+        label={{
+          label: "이름",
+          description: "이름은 입력해주세요",
+        }}
+      >
+        {({ id }) => (
+          <TextInput id={id} value={inputValue} onChange={setInputValue} />
+        )}
+      </FormField>
+
+      <FormField
+        id="birth"
+        label={{
+          label: "생일",
+          description: "생일을 입력해주세요",
+        }}
+      >
+        {({ id }) => (
+          <BirthDateInput id={id} value={birthValue} onChange={setBirthValue} />
+        )}
+      </FormField>
+
+      <CommonButton label="하이" />
     </div>
   );
 }
