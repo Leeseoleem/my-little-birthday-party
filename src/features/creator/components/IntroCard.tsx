aa10b4fa75a9a3a7d820e99762e8aa19154ea647
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-03 22:03:07 +0900

    feat: IntroCard 관련 image 파일 추가 및 컴포넌트 생성

---
+++ src/features/creator/components/IntroCard.tsx
@@ -0,0 +1,24 @@
+import CommonLinkButton from "../../../components/ui/Button/CommonLinkButton";
+
+const IntroCard = () => {
+  return (
+    <div
+      className="bg-[url('/images/paper-crumpled-white.png')]
+          bg-no-repeat
+          bg-center
+          bg-size-[100%_100%]
+          flex flex-col justify-center items-center py-15 px-6 md:px-8 xl:px-9 gap-8 md:gap-12 lg:gap-15"
+    >
+      <img
+        src="/images/title-logo.png"
+        alt="나의 작은 생일 파티"
+        className="h-[250px] md:h-[300px] lg:h-[350px] mx-auto"
+      />
+      <div className="flex w-full justify-center items-center">
+        <CommonLinkButton label="시작하기" to="/creator/info" />
+      </div>
+    </div>
+  );
+};
+
+export default IntroCard;
