e3474180dccd721ff5a89b8521478f249c5772b4
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-07 04:47:07 +0900

    design: TextBubble 스타일 조정

--- src/components/ui/Bubble/TextBubble.tsx
+++ src/components/ui/Bubble/TextBubble.tsx
@@ -13,19 +13,19 @@ interface TextBubbleProps {
 
 const TextBubble = ({ message, tail = "left-bottom" }: TextBubbleProps) => {
   const tailClass = clsx(
-    tail === "left-top" && "rounded-b-3xl rounded-tr-3xl",
-    tail === "left-bottom" && "rounded-t-3xl rounded-br-3xl",
-    tail === "right-top" && "rounded-b-3xl rounded-tl-3xl",
-    tail === "right-bottom" && "rounded-t-3xl rounded-bl-3xl"
+    tail === "left-top" && "rounded-b-4xl rounded-tr-4xl",
+    tail === "left-bottom" && "rounded-t-4xl rounded-br-4xl",
+    tail === "right-top" && "rounded-b-4xl rounded-tl-4xl",
+    tail === "right-bottom" && "rounded-t-4xl rounded-bl-4xl"
   );
   return (
     <div
       className={clsx(
-        "flex p-6 h-15 bg-gray-0 items-center shadow-bubble",
+        "flex p-6 min-h-15 max-w-[300px] bg-gray-0 items-center shadow-bubble",
         tailClass
       )}
     >
-      <p className="text-sub-title text-gray-80">{message}</p>
+      <p className="text-sub-title text-gray-80 break-keep">{message}</p>
     </div>
   );
 };
