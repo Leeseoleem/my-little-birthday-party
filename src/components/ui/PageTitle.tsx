0441c9290e154f65beffdd2b7620213f7831ef42
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-06 15:07:50 +0900

    design: 반응형 gap 요소 추가

--- src/components/ui/PageTitle.tsx
+++ src/components/ui/PageTitle.tsx
@@ -5,7 +5,7 @@ export interface PageTitleProps {
 
 const PageTitle = ({ title, subTitle }: PageTitleProps) => {
   return (
-    <div className="flex flex-col items-start gap-2">
+    <div className="flex flex-col items-start gap-1 md:gap-1.5 lg:gap-2">
       <h1 className="text-display-03 md:text-display-02 lg:text-display-01 text-main">
         {title}
       </h1>
