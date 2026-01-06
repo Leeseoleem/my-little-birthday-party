a34780908fbd7a5a51f5c578ff9701f420209372
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-06 22:38:11 +0900

    design: 전반적인 크기 조정

--- src/components/ui/Button/CarouselArrowButton.tsx
+++ src/components/ui/Button/CarouselArrowButton.tsx
@@ -19,17 +19,17 @@ const CarouselArrowButton = ({
       disabled={isDisabled}
       aria-label={direction === "left" ? "이전" : "다음"}
       className={clsx(
-        "flex justify-center items-center rounded-full w-9 h-9",
-        direction === "left" ? "pr-1" : "pl-1",
+        "flex justify-center items-center rounded-full w-6 h-6",
+        direction === "left" ? "pr-0.5" : "pl-0.5",
         isDisabled
           ? "bg-main-disabled cursor-not-allowed"
           : "bg-main hover:bg-main-hover active:bg-main-active cursor-pointer"
       )}
     >
       {direction === "left" ? (
-        <ChevronLeft size={20} className="text-gray-0" />
+        <ChevronLeft size={14} className="text-gray-0" />
       ) : (
-        <ChevronRight size={20} className="text-gray-0" />
+        <ChevronRight size={14} className="text-gray-0" />
       )}
     </button>
   );
