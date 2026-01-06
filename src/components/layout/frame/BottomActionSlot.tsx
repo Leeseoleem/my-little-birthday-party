151f293c4da66fb4f4778828c022066dc9d52c71
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-06 12:11:21 +0900

    chore: 바텀 패딩 여부 props ckdlq cnrk

--- src/components/layout/frame/BottomActionSlot.tsx
+++ src/components/layout/frame/BottomActionSlot.tsx
@@ -1,9 +1,23 @@
 import type { ReactNode } from "react";
+import clsx from "clsx";
 
 interface BottomActionSlotProps {
   children: ReactNode;
+  hasBottomPadding?: boolean;
 }
 
-export default function BottomActionSlot({ children }: BottomActionSlotProps) {
-  return <div className="mt-auto flex justify-center pb-6">{children}</div>;
+export default function BottomActionSlot({
+  children,
+  hasBottomPadding = true,
+}: BottomActionSlotProps) {
+  return (
+    <div
+      className={clsx(
+        "mt-auto flex justify-center",
+        hasBottomPadding === true && " pb-6"
+      )}
+    >
+      {children}
+    </div>
+  );
 }
