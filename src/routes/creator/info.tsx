a396d4050c9554b42cf9d14a91952e944e7963e4
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-07 16:37:01 +0900

    chore: 캐릭터 프로필 assets 추가

--- src/routes/creator/info.tsx
+++ src/routes/creator/info.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import BottomActionSlot from "../../components/layout/BottomActionSlot";
+import InvitationCreateCard from "../../features/creator/components/InvitationCreateCard";
+
+import BottomActionSlot from "../../components/layout/frame/BottomActionSlot";
 import CommonLinkButton from "../../components/ui/Button/CommonLinkButton";
 
 export const Route = createFileRoute("/creator/info")({
@@ -16,7 +18,9 @@ export const Route = createFileRoute("/creator/info")({
 function CreatorInfoPage() {
   return (
     <div className="flex flex-1 flex-col justify-center">
-      <p>수신자 정보 입력</p>
+      <div className="flex-1 flex items-center justify-center">
+        <InvitationCreateCard />
+      </div>
       <BottomActionSlot>
         <CommonLinkButton label="C3. 편지 작성으로" to={"/creator/letter"} />
       </BottomActionSlot>
