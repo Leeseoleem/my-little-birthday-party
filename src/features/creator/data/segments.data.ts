746db2a31e6c7663b9a2780644aad181d82ee8a7
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-08 18:43:52 +0900

    chore: 시작 페이지 안내를 위한 segments 데이터 추가

---
+++ src/features/creator/data/segments.data.ts
@@ -0,0 +1,17 @@
+import type { TextSegment } from "../../../components/layout/page/NoticeFrame";
+
+export const segments: TextSegment[] = [
+  { text: "시작하기를 누르면 " },
+  {
+    text: "이용약관",
+    href: "https://example.com/privacy",
+    target: "_blank",
+  },
+  { text: " 및 " },
+  {
+    text: "개인정보 처리방침",
+    href: "https://example.com/privacy",
+    target: "_blank",
+  },
+  { text: "에 동의하는 것으로 간주됩니다." },
+];
