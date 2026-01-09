e3336555315f9e730680bc22b1ed55af1cbdec01
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-10 04:25:16 +0900

    feat: info 페이지 화면 ui 구현 완료

--- src/routes/creator/info.tsx
+++ src/routes/creator/info.tsx
@@ -1,28 +1,60 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+// ----- 컴포넌트 -----
 import InvitationCreateCard from "../../features/creator/components/InvitationCreateCard";
-
 import BottomActionSlot from "../../components/layout/frame/BottomActionSlot";
-import CommonLinkButton from "../../components/ui/Button/CommonLinkButton";
+import NoticeFrame from "../../components/layout/page/NoticeFrame";
+
+// ----- 유틸 -----
+import { openInNewTab } from "../../utils/openInNewTab";
+
+// ----- 데이터 -----
+import { segments } from "../../features/creator/data/segments.data";
+import { useState } from "react";
 
 export const Route = createFileRoute("/creator/info")({
   staticData: {
     creatorHeader: {
-      value: 0.2,
-      fallbackTo: "/creator",
+      kind: "back",
     },
   },
   component: CreatorInfoPage,
 });
 
 function CreatorInfoPage() {
+  const [inviteeName, setInviteeName] = useState("");
+  const [inviteeBirthDate, setInviteeBirthDate] = useState("");
+
+  // 두 필드가 모두 채워졌을 때만 활성화
+  const isDisabled = !inviteeName.trim() || !inviteeBirthDate.trim();
+
   return (
     <div className="flex flex-1 flex-col justify-center">
       <div className="flex-1 flex items-center justify-center">
-        <InvitationCreateCard />
+        <InvitationCreateCard
+          nameField={{
+            value: inviteeName,
+            onChange: setInviteeName,
+            placeholder: "ex) 돌쇠",
+            maxLength: 10,
+          }}
+          birthDateField={{
+            id: "birthDate",
+            value: inviteeBirthDate,
+            onChange: setInviteeBirthDate,
+          }}
+        />
       </div>
       <BottomActionSlot>
-        <CommonLinkButton label="C3. 편지 작성으로" to={"/creator/letter"} />
+        <NoticeFrame
+          type="default"
+          segments={segments}
+          buttonProps={{
+            isDisabled: isDisabled,
+            label: "시작하기",
+            onClick: () => openInNewTab("/creator/letter"),
+          }}
+        />
       </BottomActionSlot>
     </div>
   );
