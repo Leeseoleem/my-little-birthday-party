2b86c9eef41fe2a79de16e2342e47c5c1b67dc0e
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-10 04:43:31 +0900

    feat: letter 페이지 편지 작성 영역 section 구현

---
+++ src/features/creator/page/letter/WriteSection.tsx
@@ -0,0 +1,33 @@
+import CreatorLetterEditor, {
+  type CreatorLetterEditorProps,
+} from "../../components/CreatorLetterEditor";
+import BottomActionSlot from "../../../../components/layout/frame/BottomActionSlot";
+import ActionButtonGroup from "../../../../components/ui/Button/ActionButtonGroup";
+
+interface WriteSectionProps {
+  editor: CreatorLetterEditorProps;
+  onClickText: () => void;
+  isDisabled?: boolean;
+}
+
+export default function WriteSection({
+  editor,
+  onClickText,
+  isDisabled = false,
+}: WriteSectionProps) {
+  return (
+    <>
+      <CreatorLetterEditor {...editor} />
+      <BottomActionSlot>
+        <ActionButtonGroup
+          onClickText={onClickText}
+          buttonProps={{
+            isDisabled: isDisabled,
+            label: "작성 완료하기",
+            to: "/creator/cake/build",
+          }}
+        />
+      </BottomActionSlot>
+    </>
+  );
+}
