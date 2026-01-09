45986473b1680acd00a550643ac46afebb5fcf42
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-09 18:16:06 +0900

    chore: 헤더 컴포넌트 export 구조 정리

---
+++ src/components/ui/Header/index.tsx
@@ -0,0 +1,5 @@
+export { default as AppHeader } from "./AppHeader";
+// ----- 헤더 전용 버튼 목록 -----
+export { BackButton } from "./BackButton";
+export { CloseButton } from "./CloseButton";
+export { ExitButton } from "./ExitButton";
