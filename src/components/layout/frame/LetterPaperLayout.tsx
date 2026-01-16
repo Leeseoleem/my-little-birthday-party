13bc27d97e8de35de57eb86c439e66853e0f73f8
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-17 01:03:14 +0900

    design: 자식 요소 중앙 정렬 추가

--- src/components/layout/frame/LetterPaperLayout.tsx
+++ src/components/layout/frame/LetterPaperLayout.tsx
@@ -102,7 +102,7 @@ export default function LetterPaperLayout({
         />
 
         <div
-          className="absolute flex flex-col justify-center"
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center"
           style={{ ...overlayStyle, ...paddingStyle }}
         >
           {children}
