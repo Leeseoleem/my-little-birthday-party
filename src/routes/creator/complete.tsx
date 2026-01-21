ece9cdedc6384e0d35de6382663a27f778b91505
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-21 15:34:54 +0900

    refactor: 재사용 컴포넌트 수정에 따른 페이지 레이아웃 수정

--- src/routes/creator/complete.tsx
+++ src/routes/creator/complete.tsx
@@ -1,8 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
+import clsx from "clsx";
 
 import GarlandLayout from "../../components/layout/page/GarlandLayout";
 import InvitationCompleteCard from "../../features/creator/components/complete/InvitationCompleteCard";
 import EnvelopeLayout from "../../components/layout/page/EnvelopeLayout";
+import { receiverPageLayout } from "../../components/shared/styles/pageLayout";
 
 export const Route = createFileRoute("/creator/complete")({
   staticData: {
@@ -19,27 +21,28 @@ export const Route = createFileRoute("/creator/complete")({
 function CreatorCompletePage() {
   return (
     <GarlandLayout hasHeader>
-      <EnvelopeLayout />
-      <div className="fixed inset-0 bg-black/4 z-5 pointer-events-none" />
-
-      <div className="flex justify-center py-8 mdh:py-16 lgh:py-25">
-        <InvitationCompleteCard
-          info={{
-            inviteeName: "이서림",
-            inviteeBirthDate: "10-14",
-          }}
-          sns={{
-            onShareKakao: () => {
-              // TODO: Kakao SDK 연동 로직
-            },
-            onShareMail: () => {
-              // TODO:  mailto 생성 로직
-            },
-            onCopyLink: () => {
-              // TODO: navigator.clipboard.writeText(url)
-            },
-          }}
-        />
+      <div className={clsx(receiverPageLayout, "px-4")}>
+        <EnvelopeLayout />
+        <div className="fixed inset-0 bg-black/4 z-5 pointer-events-none" />
+        <div className="flex my-auto justify-center py-8 mdh:py-16 lgh:py-25">
+          <InvitationCompleteCard
+            info={{
+              inviteeName: "이서림",
+              inviteeBirthDate: "10-14",
+            }}
+            sns={{
+              onShareKakao: () => {
+                // TODO: Kakao SDK 연동 로직
+              },
+              onShareMail: () => {
+                // TODO:  mailto 생성 로직
+              },
+              onCopyLink: () => {
+                // TODO: navigator.clipboard.writeText(url)
+              },
+            }}
+          />
+        </div>
       </div>
     </GarlandLayout>
   );
