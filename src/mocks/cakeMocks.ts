589495a3e11742e0cd0531549a2bb1061549793b
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-22 15:40:01 +0900

    chore: 케이크 목업 데이터 생성

---
+++ src/mocks/cakeMocks.ts
@@ -0,0 +1,11 @@
+import type { CakeDoc } from "../features/types/cake-doc.types";
+
+export const cakeDoc: CakeDoc = {
+  cakeType: "party",
+  placedCandlesBySlot: {
+    "slot-1": "long-01",
+    "slot-2": null,
+    "slot-3": "special-02",
+    "slot-4": "short-03",
+  },
+};
