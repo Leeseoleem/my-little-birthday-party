4424a7aad9153658fbca87b133fa299b5ffac960
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-24 00:38:22 +0900

    chore: 마스크 영역 크기 조정 및 left 제외

--- src/features/constant/assetDimensions.ts
+++ src/features/constant/assetDimensions.ts
@@ -8,10 +8,10 @@ export const BASE_CAKE_IMAGE_WIDTH = 900;
 export const ENVELOPE_BASE_WIDTH = 1024;
 
 // 봉투 내 마스크 영역
+
 export const MASK_BASE = {
-  left: 98,
-  top: 284,
-  width: 828,
+  top: 270,
+  width: 800,
   height: 140,
   radius: 8, // 둥글게 하고 싶으면 적당히
 };
