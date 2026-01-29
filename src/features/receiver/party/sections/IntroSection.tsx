b58e5e10dd21e085b72de11ef6929543fc463c13
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-29 13:09:35 +0900

    chore: 초기 상태 추가

--- src/features/receiver/party/sections/IntroSection.tsx
+++ src/features/receiver/party/sections/IntroSection.tsx
@@ -6,6 +6,7 @@ export default function IntroSection({ duration }: { duration: number }) {
   return (
     <motion.div
       className="absolute inset-0 z-50 flex items-center justify-center bg-black"
+      initial={{ opacity: 0.8 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: duration, ease: "easeOut" }}
