c7b31566d92d20a0ce11645a229d853ad1160fe3
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-29 13:07:04 +0900

    chore: LETTER_DUMMY 파일 실제 타입에 맞게 수정

--- src/mocks/letterMocks.ts
+++ src/mocks/letterMocks.ts
@@ -1,7 +1,7 @@
-import type { LetterPaperType } from "../features/types/letterPaper.types";
+import type { LetterDoc } from "../features/types/letterPaper.types";
 
-export const LETTER_DUMMY = {
-  paperType: "default" as LetterPaperType,
+export const LETTER_DUMMY: LetterDoc = {
+  paperType: "default",
   content:
     "생일 정말 축하해!\n" +
     "이 편지가 너에게 작은 선물이 되었으면 좋겠어.\n\n" +
