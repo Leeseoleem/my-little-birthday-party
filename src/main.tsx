97e4597a6da36df270ffe231f45330c5c3942021
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-30 21:36:59 +0900

    chore: HelmetProvider 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
-import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 
 import { router } from "./router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HelmetProvider>
-      {/* 화면 시작점: RouterProvider */}
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    {/* 화면 시작점: RouterProvider */}
+    <RouterProvider router={router} />
   </StrictMode>,
 );
