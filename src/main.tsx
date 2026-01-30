440706bcb1b436b3e6cd7dd4d65e48bad3d30c5d
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-01-30 15:50:55 +0900

    chore: HelmetProvider 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
+import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 
 import { router } from "./router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* 화면 시작점: RouterProvider */}
-    <RouterProvider router={router} />
-  </StrictMode>
+    <HelmetProvider>
+      {/* 화면 시작점: RouterProvider */}
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  </StrictMode>,
 );
