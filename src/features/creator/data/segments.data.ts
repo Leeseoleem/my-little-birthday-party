75bb40b53f1ee2221e4af817fa0f3b1bdf88eb67
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-02-13 01:21:05 +0900

    chore: 이용 약관 링크 연동

--- src/features/creator/data/segments.data.ts
+++ src/features/creator/data/segments.data.ts
@@ -1,16 +1,10 @@
 import type { TextSegment } from "../../../components/layout/page/NoticeFrame.types";
 
 export const segments: TextSegment[] = [
-  { text: "시작하기와 함께 " },
+  { text: "시작하기를 누르면 " },
   {
     text: "이용약관",
-    href: "https://example.com/privacy",
-    target: "_blank",
-  },
-  { text: " 및 " },
-  {
-    text: "개인정보 처리방침",
-    href: "https://example.com/privacy",
+    href: "https://www.notion.so/30551f6a37ed80908cf3dd4777a2e890?source=copy_link",
     target: "_blank",
   },
   { text: "에 동의하게 됩니다." },
