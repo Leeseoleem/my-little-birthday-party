c6680175a4d43847e4cc602c339506a55c1c7f95
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-02-13 19:19:40 +0900

    chore: className 값을 외부에서 받을 수 있도록 수정

--- src/components/layout/frame/ReceiptLayout.tsx
+++ src/components/layout/frame/ReceiptLayout.tsx
@@ -1,9 +1,11 @@
+import clsx from "clsx";
 import ReceiptLayoutHeader, {
   type ReceiptLayoutHeaderProps,
 } from "./ReceiptLayoutHeader";
 
 type ReceiptLayoutProps = {
   header: ReceiptLayoutHeaderProps;
+  className?: string;
   children?: React.ReactNode;
   footer?: React.ReactNode;
 };
@@ -11,12 +13,15 @@ type ReceiptLayoutProps = {
 export default function ReceiptLayout(props: ReceiptLayoutProps) {
   return (
     <div
-      className="bg-[url('/assets/textures/paper-crumpled-white.png')]
-          z-10
-          bg-no-repeat
-          bg-center
-          w-fit h-fit
-          flex flex-col justify-center items-start pb-6 pt-9 px-8 mdh:px-12 lgh:px-15 gap-6 shadow-floating"
+      className={clsx(
+        "bg-[url('/assets/textures/paper-crumpled-white.png')]",
+        "z-10 bg-no-repeat bg-center",
+        // 기본은 내용 기반이 아니라 '컨테이너가 커질 수 있게' 열어두는 편이 좋음
+        "w-fit", // 너비는 유지 (원하면 이것도 외부로 제어 가능)
+        "flex flex-col items-start",
+        "pb-6 pt-9 px-8 mdh:px-12 lgh:px-15 gap-6 shadow-floating",
+        props.className,
+      )}
     >
       <ReceiptLayoutHeader {...props.header} />
       <div className="w-full h-[1.5px] bg-gray-50" />
